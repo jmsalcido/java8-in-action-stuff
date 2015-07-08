1f7c300da63c8e8a3ecf644ed70a5bdbdddc7aad
jsalcido <jsalcido@switchfly.com>
2015-07-08 15:51:31 +0000

    Main renamed to java8stuff

--- src/main/java/otfusion/org/Java8Stuff.java
+++ src/main/java/otfusion/org/Java8Stuff.java
@@ -1,6 +1,6 @@
 package otfusion.org;
 
-public class Main {
+public class Java8Stuff {
 
     public static void main(String[] args) {
         System.out.println("Just a main class");
--- src/test/java/otfusion/org/Java8StuffTest.java
+++ src/test/java/otfusion/org/Java8StuffTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class MainTest {
+public class Java8StuffTest {
 
     @Test
     public void testJava8LambdasUsages() throws Exception {
