3b9635d08261cb4e82b9fe3e8033b998d902e96f
jmsalcido <eljose152@gmail.com>
2015-08-28 11:52:44 -0700

    meh, I forgot to commit this.

--- src/test/java/otfusion/org/Java8StuffTest.java
+++ src/test/java/otfusion/org/Java8StuffTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -28,13 +30,24 @@ public class Java8StuffTest {
 
     @Test
     public void testJava8LambdaWithStreams() throws Exception {
-        List<Integer> someNumbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        List<Integer> someNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         Integer sum = someNumbers.stream().reduce(0, (initial, addition) -> initial + addition);
-        assertEquals(Integer.valueOf(1+2+3+4+5+6+7+8+9+10), sum);
+        assertEquals(Integer.valueOf(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10), sum);
 
-        someNumbers = Arrays.asList(1,2,3,4,5);
+        someNumbers = Arrays.asList(1, 2, 3, 4, 5);
         int product = someNumbers.stream().reduce(1, (firstNumber, secondNumber) -> firstNumber * secondNumber);
         assertEquals(2 * 3 * 4 * 5, product);
+    }
+
+    public void testJava8pythagoreanTriples() throws Exception {
+        // a*a + b*b = c*c (triangles stuff)
+        Stream<int[]> triples = IntStream.rangeClosed(1, 100)
+                .boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                                .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                                .mapToObj(b ->
+                                        new int[]{a, b, (int) Math.sqrt(a * a + b * b)})
+                );
     }
 
 }
